ef526f84ee65cca1f5e98019d1c7da3342930396
achyut kadam <achyut.k@acldigital.com>
2022-10-10 13:10:00 +0530

    Book CRUD operation

--- library/images/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from images import views
-
-urlpatterns = [
-    path('', views.index, name='images'),
-    path('login', views.loginUser, name='login'),
-    path('logout', views.logoutUser, name='logout')
-]
